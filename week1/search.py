d42ea2282e648d568d1e57cfc82da7bc426238e1
christophlee <christoph@outschool.com>
2023-02-08 20:29:22 +0000

    through step 4.b.i.7

--- week1/search.py
+++ week1/search.py
@@ -94,7 +94,7 @@ def query():
     print("query obj: {}".format(query_obj))
 
     #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
+    response = opensearch.search(body=query_obj, index='bbuy_products')   # TODO: Replace me with an appropriate call to OpenSearch
     # Postprocess results here if you so desire
 
     #print(response)
@@ -111,11 +111,47 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "bool": {
+                "must": {
+                    "query_string": {
+                        "query": user_query,
+                        "fields": ["name", "shortDescription", "longDescription"],
+                        "phrase_slop": 3
+                    },
+                },
+                "filter": filters
+            },
         },
         "aggs": {
             #### Step 4.b.i: create the appropriate query and aggregations here
-
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        { "to": 10.0 },
+                        { "from": 10.0, "to": 15.0 },
+                        { "from": 15.0, "to": 20.0 },
+                        { "from": 20.0, "to": 30.0 },
+                        { "from": 30.0, "to": 50.0 },
+                        { "from": 50.0, "to": 100.0 },
+                        { "from": 100.0, "to": 200.0 },
+                        { "from": 200.0 }
+                    ]
+                }
+            },
+            "department": {
+                "terms": {
+                    "field": "department.keyword",
+                    "size": 10,
+                    "min_doc_count": 10
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image.keyword"
+                }
+            }
         }
     }
+
     return query_obj
